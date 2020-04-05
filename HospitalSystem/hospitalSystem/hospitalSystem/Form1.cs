ef6da0181bbf0aa566c95e79967342f821628566
mayarkhaled <miarkhaled3@gmail.com>
2020-04-05 16:01:10 +0200

    creating new project "vs2017" and removing "vs 2019"

--- HospitalSystem/hospitalSystem/hospitalSystem/Form1.cs
+++ HospitalSystem/hospitalSystem/hospitalSystem/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace dataBase
+namespace hospitalSystem
 {
     public partial class Form1 : Form
     {
@@ -16,6 +16,5 @@ namespace dataBase
         {
             InitializeComponent();
         }
-
     }
 }
