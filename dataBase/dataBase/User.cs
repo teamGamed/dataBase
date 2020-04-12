0361b6c220531051512d4ad6a08ea3d412ac2469
mahmoodsherif <ma13121999@gmail.com>
2020-04-12 19:55:55 +0200

    add User class

---
+++ dataBase/dataBase/User.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataBase
+{
+    public class User
+    {
+        private int id;
+        private string username;
+        private string password;
+        private string name;
+        private string email;
+        private string phone;
+        private string address;
+        private string photo_url;
+        private string type;
+        private string sex;
+
+        public int Id
+        {
+            get => id;
+            set => id = value;
+        }
+
+        public string Username
+        {
+            get => username;
+            set => username = value;
+        }
+
+        public string Password
+        {
+            get => password;
+            set => password = value;
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public string Email
+        {
+            get => email;
+            set => email = value;
+        }
+
+        public string Phone
+        {
+            get => phone;
+            set => phone = value;
+        }
+
+        public string Address
+        {
+            get => address;
+            set => address = value;
+        }
+
+        public string PhotoUrl
+        {
+            get => photo_url;
+            set => photo_url = value;
+        }
+
+        public string Type
+        {
+            get => type;
+            set => type = value;
+        }
+
+        public string Sex
+        {
+            get => sex;
+            set => sex = value;
+        }
+    }
+}
