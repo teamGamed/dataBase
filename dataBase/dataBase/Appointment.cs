f85cde00c73425a56becd70389570d85b8d3f66d
mahmoodsherif <ma13121999@gmail.com>
2020-04-13 20:50:06 +0200

    add appointment

---
+++ dataBase/dataBase/Appointment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataBase
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+
+        public string DoctorUsername { get; set; }
+
+        public string PatientUsername { get; set; }
+
+        public string Date { get; set; }
+
+        public string Room { get; set; }
+
+        protected bool Equals(Appointment other)
+        {
+            return DoctorUsername == other.DoctorUsername && PatientUsername == other.PatientUsername && Date == other.Date && Room == other.Room;
+        }
+
+    }
+}
