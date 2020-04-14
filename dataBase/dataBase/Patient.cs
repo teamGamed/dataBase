2d6d3cada4fe79d6c252508a2a10ef914061aa0f
mahmoodSherif <mahmoodSherif13@gmail.com>
2020-04-14 09:22:34 +0200

    add patient class

---
+++ dataBase/dataBase/Patient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dataBase.db;
+
+namespace dataBase
+{
+    class Patient:User
+    {
+        public string Weight { get; set; }
+
+        public string Height { get; set; }
+
+        public string Birthday { get; set; }
+
+        public string BloodType { get; set; }
+
+        public string University { get; set; }
+
+        public List<Appointment> Appointments { get; }
+
+        public Patient()
+        {
+
+        }
+
+        public Patient(User user)
+        {
+            Copy(user);
+            Appointments = dbPatient.GetAllAppointments(this.Username);
+        }
+
+        protected bool Equals(Patient other)
+        {
+            return base.Equals(other) && Weight == other.Weight && Height == other.Height && Birthday == other.Birthday && BloodType == other.BloodType && University == other.University && Equals(Appointments, other.Appointments);
+        }
+
+        
+    }
+}
