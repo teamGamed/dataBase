6bb1c3ee5403e7d91590b4bfe115c873d780bf93
MohamedAdel769 <mohamed.adel90211@gmail.com>
2020-04-21 15:21:31 +0200

    a

---
+++ dataBase/dataBase/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using dataBase.db;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+
+
+
+namespace dataBase
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            for(int i = 0; i< dbDisease.GetAllDisease().Count; i++)
+            {
+                comboBox1.Items.Add(dbDisease.GetAllDisease()[i].Name);
+            }
+        }
+
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+        }
+    }
+}
