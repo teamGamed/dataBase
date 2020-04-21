f88de6f3375b9720ceb6b139cef27e494c6772ab
MohamedAdel769 <mohamed.adel90211@gmail.com>
2020-04-21 15:08:39 +0200

    Done

--- dataBase/dataBase/Reports.Designer.cs
+++ dataBase/dataBase/Reports.Designer.cs
@@ -30,15 +30,17 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(425, 23);
+            this.button1.Location = new System.Drawing.Point(245, 23);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(127, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Generate report";
+            this.button1.Text = "Appointments reports";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -52,11 +54,33 @@
             this.crystalReportViewer1.Size = new System.Drawing.Size(884, 443);
             this.crystalReportViewer1.TabIndex = 1;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(455, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "examinations report";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(651, 23);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(193, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "patients diseases report";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Reports
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1034, 526);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.crystalReportViewer1);
             this.Controls.Add(this.button1);
             this.Name = "Reports";
@@ -70,5 +94,7 @@
 
         private System.Windows.Forms.Button button1;
         private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- dataBase/dataBase/Reports.cs
+++ dataBase/dataBase/Reports.cs
@@ -13,6 +13,9 @@ namespace dataBase
     public partial class Reports : Form
     {
         patientExamination rep1;
+        appointmentsReport rep2;
+        patientdiseaseReport rep3;
+
         public Reports()
         {
             InitializeComponent();
@@ -20,12 +23,24 @@ namespace dataBase
 
         private void button1_Click(object sender, EventArgs e)
         {
-            crystalReportViewer1.ReportSource = rep1;
+            crystalReportViewer1.ReportSource = rep2;
         }
 
         private void Reports_Load(object sender, EventArgs e)
         {
             rep1 = new patientExamination();
+            rep2 = new appointmentsReport();
+            rep3 = new patientdiseaseReport();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            crystalReportViewer1.ReportSource = rep1;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            crystalReportViewer1.ReportSource = rep3;
         }
     }
 }
